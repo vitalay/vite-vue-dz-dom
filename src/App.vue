a8ab73da09ba40202a4b857a67004d1a67de020b
vitalya <vitalay.efimov@gmail.com>
2025-05-02 09:06:16 +0500

    one commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const formattedDate = ref('');
+
+onMounted(() => {
+  formattedDate.value = new Date().toLocaleString('ru-RU', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+});
+</script>
+
+<template>
+  <div>
+    <h1>Сегодня:</h1>
+    <p class="date">{{ formattedDate }}</p>
+  </div>
+</template>
+
+<style scoped>
+.date {
+  font-size: 30px;
+}
+</style>
